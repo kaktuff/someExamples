a3e8136050762dcd9b1c68d9b7199c40ab385a70
kaktuff <poskotinova.lyubov@gmail.com>
2016-05-30 17:24:24 +0700

    создан класс App - формочка

---
+++ src/someExamples/App.java
@@ -0,0 +1,54 @@
+package someExamples;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by poskotinova-ls on 30.05.2016.
+ * это главный класс из которого будет запускаться приложение
+ */
+public class App {
+    public static void main(String[] args) {
+        JFrame mainJFrame = new JFrame();
+        mainJFrame.setTitle("Sort File");
+        mainJFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainJFrame.setSize(500, 400);
+        mainJFrame.setBackground(Color.GREEN);
+        mainJFrame.setLocationRelativeTo(null);
+        mainJFrame.setPreferredSize(new Dimension(500, 400));
+
+        //Container contentPane = new Container();
+        Container contentPane = mainJFrame.getContentPane();
+        // тут есть ещё другие layout, для красоты как бы лучше наверное что-то другое
+        //contentPane.setLayout(new BorderLayout());
+        //contentPane.add(new JButton("Select File"), BorderLayout.WEST);
+        //contentPane.add(new JButton("Select File"), BorderLayout.SOUTH);
+        contentPane.setLayout(new GridBagLayout());
+        /*
+        contentPane.add(new JButton("Select File1"),2);
+        contentPane.add(new JButton("Select File2"));
+        contentPane.add(new JButton("Select File3"));
+        contentPane.add(new JButton("Select File4"));
+        contentPane.add(new JButton("Select File5"));
+        contentPane.add(new JButton("Select File6"));*/
+
+        // TO_DO: разобраться с этим!
+        JButton button;
+        GridBagConstraints c = new GridBagConstraints();
+
+        button = new JButton("Button 1");
+        //c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridwidth = 3;
+        c.weightx = 3;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.weightx = 2;
+        c.weighty = 2;
+        contentPane.add(button, c);
+
+        //mainJFrame.add(contentPane);
+
+        mainJFrame.pack();
+        mainJFrame.setVisible(true);
+    }
+}
